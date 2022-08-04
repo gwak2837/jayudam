108fd58c5f9a81f74cc899a70da5442151c5d9e0
Taeuk Gwak <gwak2837@kakao.com>
2022-08-04 13:42:49 +0900

    Upgrade packages

--- src/pages/post/[id]/index.tsx
+++ src/pages/post/[id]/index.tsx
@@ -1,8 +1,13 @@
 import { useRouter } from 'next/router'
+import Image from 'next/future/image'
 import React from 'react'
 import { toastApolloError } from 'src/apollo/error'
 import PageHead from 'src/components/PageHead'
 import { usePostQuery } from 'src/graphql/generated/types-and-hooks'
+import Navigation from 'src/layouts/Navigation'
+import styled from 'styled-components'
+import BackArrowIcon from 'src/svgs/back-arrow.svg'
+import ThreeDotsIcon from 'src/svgs/three-dots.svg'
 
 const description = ''
 
@@ -16,9 +21,63 @@ export default function PostPage() {
     variables: { id: postId },
   })
 
+  const parentPost = data?.post
+
   return (
-    <PageHead title={`{제목} - 자유담`} description={description}>
-      <pre style={{ margin: 0, overflow: 'auto' }}>{JSON.stringify(data, null, 2)}</pre>
+    <PageHead
+      title={`${parentPost?.author?.nickname ?? '이름'} - 자유담`}
+      description={description}
+    >
+      <Navigation>
+        <Sticky>
+          <BackArrowIcon />
+          이야기
+        </Sticky>
+        <Padding>
+          <FlexBetween>
+            <Image
+              src={parentPost?.author?.imageUrl ?? '/images/shortcut-icon.webp'}
+              alt="profile"
+              // fill
+              sizes="100vw"
+            />
+            <div>
+              <span>{parentPost?.author?.nickname}</span>
+              <span>{parentPost?.author?.name}</span>
+            </div>
+            <ThreeDotsIcon />
+          </FlexBetween>
+        </Padding>
+        <pre style={{ margin: 0, overflow: 'auto' }}>{JSON.stringify(data, null, 2)}</pre>
+      </Navigation>
     </PageHead>
   )
 }
+
+const Sticky = styled.div`
+  position: sticky;
+  top: 0;
+
+  display: flex;
+  align-items: center;
+  background: #fff;
+  padding: 0 1rem;
+
+  svg {
+    width: 3rem;
+    padding: 0.5rem 1rem 0.5rem 0;
+  }
+`
+
+const Padding = styled.div`
+  padding: 1rem;
+`
+
+const FlexBetween = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  > svg {
+    width: 2rem;
+  }
+`
